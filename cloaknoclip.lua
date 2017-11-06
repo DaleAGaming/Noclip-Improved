f97955d12f1d2d038b0b1bd34e6ef7a40c55fb3d
Aidan Nellyer <daleagaming@gmail.com>
2017-11-06 19:47:49 +0000

    2.1
    Quick bug fix, error when cloaking. However, it only works with serverguard now.

--- cloaknoclip.lua
+++ cloaknoclip.lua
@@ -1,11 +1,10 @@
 local function NoclipCloak( ply )
 
-    if !ply:GetMoveType() == MOVETYPE_NOCLIP then
-        ply:SetColor(Color(0, 0, 0, 0))
+if serverguard.command.Run(player,invisible, bIsSilent)
+   
 DarkRP.notify( ply, 0, 10, "You are now cloaked!" )
     else
-        ply:SetColor(Color(255,255,255,255))
-DarkRP.notify( ply, 0, 10, "You are no longer cloaked" )
+DarkRP.notify( ply, 0, 10, "You are no longer cloaked!" )
     end
 return true
 
