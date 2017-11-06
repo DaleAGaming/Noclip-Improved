3300f31bbc55d8119e639fa775bdd78203f72627
Aidan Nellyer <daleagaming@gmail.com>
2017-11-06 19:09:47 +0000

    2.0
    Added a notification for noclipping and un-noclipping
    
    Fixed issue with notifications not ending
    
    WARNING: Only works with noclipping at the moment

--- cloaknoclip.lua
+++ cloaknoclip.lua
@@ -1,13 +1,14 @@
 local function NoclipCloak( ply )
 
-	if !ply:GetMoveType() == MOVETYPE_NOCLIP then
-		ply:SetColor(Color(0, 0, 0, 0))
-	else
-		ply:SetColor(Color(255,255,255,255))
-	end
+    if !ply:GetMoveType() == MOVETYPE_NOCLIP then
+        ply:SetColor(Color(0, 0, 0, 0))
+DarkRP.notify( ply, 0, 10, "You are now cloaked!" )
+    else
+        ply:SetColor(Color(255,255,255,255))
+DarkRP.notify( ply, 0, 10, "You are no longer cloaked" )
+    end
 return true
-end
 
-DarkRP.notify( ply, 0, 10, "You are now noclipping and are cloaked!" )
+end
 
 hook.Add("PlayerNoClip", "NoclipCloak", NoclipCloak)
